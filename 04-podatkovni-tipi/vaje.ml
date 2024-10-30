46cbdce72c1c220392b8e25d3fc748764bbea072
Lara Velkavrh <lv31155@student.uni-lj.si>
2024-10-30 12:49:53 +0100

    tipi vaje

--- 04-podatkovni-tipi/vaje.ml
+++ 04-podatkovni-tipi/vaje.ml
@@ -1,151 +1,160 @@
 (*----------------------------------------------------------------------------*
- # Podatkovni tipi
-[*----------------------------------------------------------------------------*)
+   # Podatkovni tipi
+  [*----------------------------------------------------------------------------*)
 
 (*----------------------------------------------------------------------------*
- ## Valute
+   ## Valute
 
- Pri modeliranju denarja ponavadi uporabljamo racionalna števila. Problemi se
- pojavijo, ko uvedemo različne valute. Oglejmo si dva pristopa k izboljšavi
- varnosti pri uporabi valut.
-[*----------------------------------------------------------------------------*)
+   Pri modeliranju denarja ponavadi uporabljamo racionalna števila. Problemi se
+   pojavijo, ko uvedemo različne valute. Oglejmo si dva pristopa k izboljšavi
+   varnosti pri uporabi valut.
+  [*----------------------------------------------------------------------------*)
 
 (*----------------------------------------------------------------------------*
- Definirajte tipa `euro` in `dollar`, kjer ima vsak od tipov zgolj en
- konstruktor, ki sprejme racionalno število. Nato napišite funkciji
- `euro_to_dollar` in `dollar_to_euro`, ki primerno pretvarjata valuti (točne
- vrednosti pridobite na internetu ali pa si jih izmislite).
+   Definirajte tipa `euro` in `dollar`, kjer ima vsak od tipov zgolj en
+   konstruktor, ki sprejme racionalno število. Nato napišite funkciji
+   `euro_to_dollar` in `dollar_to_euro`, ki primerno pretvarjata valuti (točne
+   vrednosti pridobite na internetu ali pa si jih izmislite).
 
- Namig: Občudujte informativnost tipov funkcij.
-[*----------------------------------------------------------------------------*)
+   Namig: Občudujte informativnost tipov funkcij.
+  [*----------------------------------------------------------------------------*)
 
-type euro 
+type euro = Euro of float
+type dollar = Dollar of float
 
-type dollar 
-
-let dollar_to_euro _ = ()
-
-let euro_to_dollar _ = ()
-
+let dollar_to_euro : dollar -> euro = fun (Dollar m) -> Euro (0.9249 *. m)
+let euro_to_dollar : euro -> dollar = fun (Euro m) -> Dollar (1.0814 *. m)
 (* let primer_valute_1 = dollar_to_euro (Dollar 0.5) *)
 (* val primer_valute_1 : euro = Euro 0.4305 *)
 
 (*----------------------------------------------------------------------------*
- Definirajte tip `currency` kot en vsotni tip z konstruktorji za jen, funt in
- švedsko krono. Nato napišite funkcijo `to_pound`, ki primerno pretvori valuto
- tipa `currency` v funte.
+   Definirajte tip `currency` kot en vsotni tip z konstruktorji za jen, funt in
+   švedsko krono. Nato napišite funkcijo `to_pound`, ki primerno pretvori valuto
+   tipa `currency` v funte.
 
- Namig: V tip dodajte še švicarske franke in se navdušite nad dejstvom, da vas
- Ocaml sam opozori, da je potrebno popraviti funkcijo `to_pound`.
-[*----------------------------------------------------------------------------*)
+   Namig: V tip dodajte še švicarske franke in se navdušite nad dejstvom, da vas
+   Ocaml sam opozori, da je potrebno popraviti funkcijo `to_pound`.
+  [*----------------------------------------------------------------------------*)
 
-type currency 
+type currency = Yen of float | Pound of float | SwedishCrown of float
 
-let to_pound _ = ()
+let to_pound : currency -> currency =
+ fun m ->
+  match m with
+  | Yen k -> Pound (k *. 0.0050)
+  | Pound k -> m
+  | SwedishCrown k -> Pound (k *. 0.0727)
 
 (* let primer_valute_2 = to_pound (Yen 100.) *)
 (* val primer_valute_2 : currency = Pound 0.700000000000000067 *)
 
 (*----------------------------------------------------------------------------*
- ## Mešani seznami
+   ## Mešani seznami
 
- Želimo uporabljati sezname, ki hranijo tako cela števila kot tudi logične
- vrednosti. To bi lahko rešili tako da uvedemo nov tip, ki predstavlja celo
- število ali logično vrednost, v nadaljevanju pa bomo raje konstruirali nov tip
- seznamov.
+   Želimo uporabljati sezname, ki hranijo tako cela števila kot tudi logične
+   vrednosti. To bi lahko rešili tako da uvedemo nov tip, ki predstavlja celo
+   število ali logično vrednost, v nadaljevanju pa bomo raje konstruirali nov tip
+   seznamov.
 
- Spomnimo se, da lahko tip `list` predstavimo s konstruktorjem za prazen seznam
- `Nil`(oz. `[]` v Ocamlu) in pa konstruktorjem za člen `Cons(x, xs)` (oz. `x ::
- xs` v Ocamlu).
-[*----------------------------------------------------------------------------*)
-
-(*----------------------------------------------------------------------------*
- Definirajte tip `intbool_list` z konstruktorji za:
-
- - prazen seznam,
- - člen s celoštevilsko vrednostjo,
- - člen z logično vrednostjo.
-
- Nato napišite testni primer, ki bi predstavljal `[5; true; false; 7]`.
-[*----------------------------------------------------------------------------*)
-
-type intbool_list 
-
-let test = ()
+   Spomnimo se, da lahko tip `list` predstavimo s konstruktorjem za prazen seznam
+   `Nil`(oz. `[]` v Ocamlu) in pa konstruktorjem za člen `Cons(x, xs)` (oz. `x ::
+   xs` v Ocamlu).
+  [*----------------------------------------------------------------------------*)
 
 (*----------------------------------------------------------------------------*
- Funkcija `intbool_map f_int f_bool ib_list` preslika vrednosti `ib_list` v nov
- `intbool_list` seznam, kjer na elementih uporabi primerno od funkcij `f_int`
- oz. `f_bool`.
-[*----------------------------------------------------------------------------*)
+   Definirajte tip `intbool_list` z konstruktorji za:
 
-let rec intbool_map _ _ _ = ()
+   - prazen seznam,
+   - člen s celoštevilsko vrednostjo,
+   - člen z logično vrednostjo.
+
+   Nato napišite testni primer, ki bi predstavljal `[5; true; false; 7]`.
+  [*----------------------------------------------------------------------------*)
+
+type intbool_list =
+  | Nil
+  | Int of int * intbool_list
+  | Bool of bool * intbool_list
+
+let test = Int (5, Bool (true, Bool (false, Int (7, Nil))))
 
 (*----------------------------------------------------------------------------*
- Funkcija `intbool_reverse` obrne vrstni red elementov `intbool_list` seznama.
- Funkcija je repno rekurzivna.
-[*----------------------------------------------------------------------------*)
+   Funkcija `intbool_map f_int f_bool ib_list` preslika vrednosti `ib_list` v nov
+   `intbool_list` seznam, kjer na elementih uporabi primerno od funkcij `f_int`
+   oz. `f_bool`.
+  [*----------------------------------------------------------------------------*)
 
-let rec intbool_reverse _ = ()
+let rec intbool_map f_int f_bool = function
+  | Nil -> Nil
+  | Int (i, ib_list) -> Int (f_int i, intbool_map f_int f_bool ib_list)
+  | Bool (b, ib_list) -> Bool (f_bool b, intbool_map f_int f_bool ib_list)
 
 (*----------------------------------------------------------------------------*
- Funkcija `intbool_separate ib_list` loči vrednosti `ib_list` v par `list`
- seznamov, kjer prvi vsebuje vse celoštevilske vrednosti, drugi pa vse logične
- vrednosti. Funkcija je repno rekurzivna in ohranja vrstni red elementov.
-[*----------------------------------------------------------------------------*)
+   Funkcija `intbool_reverse` obrne vrstni red elementov `intbool_list` seznama.
+   Funkcija je repno rekurzivna.
+  [*----------------------------------------------------------------------------*)
+
+let rec intbool_reverse ib_list =
+   let rec pomozni_reverse ib_list acc =
+      match ib_list with
+      | Nil -> Nil
+      | 
+
+(*----------------------------------------------------------------------------*
+   Funkcija `intbool_separate ib_list` loči vrednosti `ib_list` v par `list`
+   seznamov, kjer prvi vsebuje vse celoštevilske vrednosti, drugi pa vse logične
+   vrednosti. Funkcija je repno rekurzivna in ohranja vrstni red elementov.
+  [*----------------------------------------------------------------------------*)
 
 let rec intbool_separate _ = ()
 
 (*----------------------------------------------------------------------------*
- ## Čarodeji
+   ## Čarodeji
 
- Določeni ste bili za vzdrževalca baze podatkov za svetovno priznano čarodejsko
- akademijo "Effemef". Vaša naloga je konstruirati sistem, ki bo omogočil
- pregledno hranjenje podatkov.
-[*----------------------------------------------------------------------------*)
-
-(*----------------------------------------------------------------------------*
- Čarodeje razvrščamo glede na vrsto magije, ki se ji posvečajo. Definirajte tip
- `magic`, ki loči med magijo ognja, magijo ledu in magijo arkane oz. fire, frost
- in arcane. Ko se čarodej zaposli na akademiji, se usmeri v zgodovino,
- poučevanje ali raziskovanje oz. historian, teacher in researcher. Definirajte
- tip `specialisation`, ki loči med temi zaposlitvami.
-[*----------------------------------------------------------------------------*)
-
-type magic 
-
-type specialisation 
+   Določeni ste bili za vzdrževalca baze podatkov za svetovno priznano čarodejsko
+   akademijo "Effemef". Vaša naloga je konstruirati sistem, ki bo omogočil
+   pregledno hranjenje podatkov.
+  [*----------------------------------------------------------------------------*)
 
 (*----------------------------------------------------------------------------*
- Vsak od čarodejev začne kot začetnik, nato na neki točki postane študent, na
- koncu pa SE lahko tudi zaposli. Definirajte tip `status`, ki določa ali je
- čarodej:
+   Čarodeje razvrščamo glede na vrsto magije, ki se ji posvečajo. Definirajte tip
+   `magic`, ki loči med magijo ognja, magijo ledu in magijo arkane oz. fire, frost
+   in arcane. Ko se čarodej zaposli na akademiji, se usmeri v zgodovino,
+   poučevanje ali raziskovanje oz. historian, teacher in researcher. Definirajte
+   tip `specialisation`, ki loči med temi zaposlitvami.
+  [*----------------------------------------------------------------------------*)
 
- - začetnik `Newbie`,
- - študent `Student` (in kateri vrsti magije pripada in koliko časa študira),
- - zaposlen `Employed` (in vrsto magije in specializacijo).
-
- Nato definirajte zapisni tip `wizard` z poljem za ime in poljem za trenuten
- status ter dodajte primer `professor`, ki je zaposlen učitelj magije ognja, in
- `jaina`, ki je četrto leto študentka magije ledu.
-[*----------------------------------------------------------------------------*)
-
-type status 
-
-type wizard 
-
-let professor  = ()
-
-let jaina  = ()
+type magic
+type specialisation
 
 (*----------------------------------------------------------------------------*
- Želimo prešteti koliko uporabnikov posamezne od vrst magije imamo na akademiji.
- Definirajte zapisni tip `magic_counter`, ki v posameznem polju hrani število
- uporabnikov magije. Nato definirajte funkcijo `update counter magic`, ki vrne
- nov števec s posodobljenim poljem glede na vrednost `magic`.
-[*----------------------------------------------------------------------------*)
+   Vsak od čarodejev začne kot začetnik, nato na neki točki postane študent, na
+   koncu pa SE lahko tudi zaposli. Definirajte tip `status`, ki določa ali je
+   čarodej:
 
-type magic_counter 
+   - začetnik `Newbie`,
+   - študent `Student` (in kateri vrsti magije pripada in koliko časa študira),
+   - zaposlen `Employed` (in vrsto magije in specializacijo).
+
+   Nato definirajte zapisni tip `wizard` z poljem za ime in poljem za trenuten
+   status ter dodajte primer `professor`, ki je zaposlen učitelj magije ognja, in
+   `jaina`, ki je četrto leto študentka magije ledu.
+  [*----------------------------------------------------------------------------*)
+
+type status
+type wizard
+
+let professor = ()
+let jaina = ()
+
+(*----------------------------------------------------------------------------*
+   Želimo prešteti koliko uporabnikov posamezne od vrst magije imamo na akademiji.
+   Definirajte zapisni tip `magic_counter`, ki v posameznem polju hrani število
+   uporabnikov magije. Nato definirajte funkcijo `update counter magic`, ki vrne
+   nov števec s posodobljenim poljem glede na vrednost `magic`.
+  [*----------------------------------------------------------------------------*)
+
+type magic_counter
 
 let update _ _ = ()
 
@@ -153,9 +162,9 @@ let update _ _ = ()
 (* val primer_carovniki_1 : magic_counter = {fire = 1; frost = 1; arcane = 2} *)
 
 (*----------------------------------------------------------------------------*
- Funkcija `count_magic` sprejme seznam čarodejev in vrne števec uporabnikov
- različnih vrst magij.
-[*----------------------------------------------------------------------------*)
+   Funkcija `count_magic` sprejme seznam čarodejev in vrne števec uporabnikov
+   različnih vrst magij.
+  [*----------------------------------------------------------------------------*)
 
 let count_magic _ = ()
 
@@ -163,17 +172,17 @@ let count_magic _ = ()
 (* val primer_carovniki_2 : magic_counter = {fire = 3; frost = 0; arcane = 0} *)
 
 (*----------------------------------------------------------------------------*
- Želimo poiskati primernega kandidata za delovni razpis. Študent lahko postane
- zgodovinar po vsaj treh letih študija, raziskovalec po vsaj štirih letih
- študija in učitelj po vsaj petih letih študija. Funkcija `find_candidate magic
- specialisation wizard_list` poišče prvega primernega kandidata na seznamu
- čarodejev in vrne njegovo ime, čim ustreza zahtevam za `specialisation` in
- študira vrsto `magic`. V primeru, da ni primernega kandidata, funkcija vrne
- `None`.
-[*----------------------------------------------------------------------------*)
+   Želimo poiskati primernega kandidata za delovni razpis. Študent lahko postane
+   zgodovinar po vsaj treh letih študija, raziskovalec po vsaj štirih letih
+   študija in učitelj po vsaj petih letih študija. Funkcija `find_candidate magic
+   specialisation wizard_list` poišče prvega primernega kandidata na seznamu
+   čarodejev in vrne njegovo ime, čim ustreza zahtevam za `specialisation` in
+   študira vrsto `magic`. V primeru, da ni primernega kandidata, funkcija vrne
+   `None`.
+  [*----------------------------------------------------------------------------*)
 
 let find_candidate _ _ _ = ()
 
 (* let primer_carovniki_3 =
-  find_candidate Frost Researcher [professor; jaina] *)
+   find_candidate Frost Researcher [professor; jaina] *)
 (* val primer_carovniki_3 : string option = Some "Jaina" *)
